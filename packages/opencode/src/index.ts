fb60c8ed5b159183e3eccaa33abf017457f6e3a7
Haris Gušić <harisgusic.dev@gmail.com>
2025-10-10 20:32:27 +0200

    fix: Explicitly exit process to prevent hanging subprocesses

--- packages/opencode/src/index.ts
+++ packages/opencode/src/index.ts
@@ -135,6 +135,12 @@ try {
     console.error(e)
   }
   process.exitCode = 1
+} finally {
+  // Some subprocesses don't react properly to SIGTERM and similar signals.
+  // Most notably, some docker-container-based MCP servers don't handle such signals unless
+  // run using `docker run --init`.
+  // Explicitly exit to avoid any hanging subprocesses.
+  process.exit();
 }
 
 cancel.abort()
