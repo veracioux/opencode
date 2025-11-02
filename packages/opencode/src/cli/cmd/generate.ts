c92adcacb17c9782805834866d811422a0c15e07
Haris Gušić <harisgusic.dev@gmail.com>
2025-11-02 10:28:37 +0100

    feat: Add EOL to generate subcommand

--- packages/opencode/src/cli/cmd/generate.ts
+++ packages/opencode/src/cli/cmd/generate.ts
@@ -1,10 +1,11 @@
 import { Server } from "../../server/server"
 import type { CommandModule } from "yargs"
+import { EOL } from "os"
 
 export const GenerateCommand = {
   command: "generate",
   handler: async () => {
     const specs = await Server.openapi()
-    process.stdout.write(JSON.stringify(specs, null, 2))
+    process.stdout.write(JSON.stringify(specs, null, 2) + EOL)
   },
 } satisfies CommandModule
